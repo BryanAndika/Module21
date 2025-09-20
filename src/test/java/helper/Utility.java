126920d6775be06a6db58234212bbf28ceaa1d74
BryanAndika <bryan.andika77@gmail.com>
2025-09-20 23:51:43 +0700

    update gradle.yml and utility

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -3,6 +3,7 @@ package helper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Utility {
 
@@ -10,14 +11,26 @@ public class Utility {
 
     // membuka google chrome
     public static void startWebDriver() {
+        // kalau pakai WebDriverManager tetap jalan
         WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().fullscreen();
+
+        // ini biar jalan di GitHub Actions (Linux runner)
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        webDriver = new ChromeDriver(options);
+        webDriver.manage().window().maximize();
         webDriver.get("https://www.saucedemo.com/v1/index.html");
     }
 
     // menutup google chrome
     public static void quitDriver() {
-        webDriver.quit();
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 }
