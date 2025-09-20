e991bfdcadbab2ee435ab1ed36a80d63d16fe370
BryanAndika <102016192+BryanAndika@users.noreply.github.com>
2025-09-20 22:36:37 +0700

    Update Utility.java

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -3,21 +3,28 @@ package helper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Utility {
 
     public static WebDriver webDriver;
 
-    //membuka google chrome
-    public static void startWebDriver(){
+    // membuka google chrome
+    public static void startWebDriver() {
         WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().fullscreen();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Tambahan supaya jalan di CI/CD (GitHub Actions)
+        options.addArguments("--headless=new");  // penting: jalan tanpa UI
+        options.addArguments("--no-sandbox");    // supaya bisa jalan di container
+        options.addArguments("--disable-dev-shm-usage");
+
+        webDriver = new ChromeDriver(options);
+        // Jangan pakai fullscreen di headless, bisa diganti maximize
+        webDriver.manage().window().maximize();
+
         webDriver.get("https://www.saucedemo.com/v1/index.html");
     }
 
-    //menutup google chrome
-    public static void quitDriver(){
-        webDriver.quit();
-    }
-}
+    // me
